e3c2db918943ac18840caf559122eaa556e809af
Nik2006123 <141873498+Nik2006123@users.noreply.github.com>
2023-08-18 07:38:43 -0400

    Add files via upload

---
+++ homwork/homworl 11.py
@@ -0,0 +1,16 @@
+
+Name_list = ["N","i","k","a", "T", "A", "k", "i", "d", "z", "e" ] 
+print([Name_list[1]])
+print([Name_list[3]])
+print([Name_list[5]])
+print([Name_list[10]])
+
+
+familylist = "Nika","shorena","Besik", "Takidze", "Dvali", "Takidze"
+print([familylist[0]])
+print([familylist[1]])
+print([familylist[2]])
+print([familylist[3]])
+print([familylist[4]])
+print([familylist[5]])
+print([familylist[6]])
